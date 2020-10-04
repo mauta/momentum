c4c025a6bfd90a90d6eb48cdd66f05e4668239a8
mauta <anna.opareva@gmail.com>
2020-10-04 14:15:25 +0300

    fix: fix some code

--- momentum/script.js
+++ momentum/script.js
@@ -1,5 +1,5 @@
  const time = document.querySelector('.time')
- const   day = document.querySelector('.day')
+ const day = document.querySelector('.day')
  const greeting = document.querySelector('.greeting')
  const bgReload = document.querySelector('.bg-reload')
  const blockquote = document.querySelector('blockquote')
@@ -17,147 +17,155 @@
  const cityInput = document.querySelector('.city_input')
  const cityText = document.querySelector('.city_text')
 
-   function showTime() {
-     const today = new Date()
-     const  year = today.getFullYear()
-     const hour = today.getHours()
-     const min = today.getMinutes()
-     const sec = today.getSeconds();
-     day.innerHTML = `${rusMonth()} ${year}`;
-     time.innerHTML = `<span class="time_block">  ${hour} </span> <span> : </span><span class="time_block">${addZerro(min)} </span> <span> : </span><span class="time_block"> ${addZerro(sec)} </span>`
-     setTimeout(showTime, 1000);
-   }
- 
+ function showTime() {
+   const today = new Date()
+   const year = today.getFullYear()
+   const hour = today.getHours()
+   const min = today.getMinutes()
+   const sec = today.getSeconds()
+   day.innerHTML = `${rusMonth()} ${year}`
+   time.innerHTML = `<span class="time_block">  ${hour} </span> <span> : </span><span class="time_block">${addZerro(min)} </span> <span> : </span><span class="time_block"> ${addZerro(sec)} </span>`
+   setTimeout(showTime, 1000)
+ }
+
  function addZerro(n) {
    if (n < 10) {
      return `0${n}`
-   } else return n;
+   } else return n
  }
 
  function rusMonth() {
-  const date = new Date();
-  const options = { day: 'numeric', month: 'long' };
-  return date.toLocaleDateString('ru-RU', options);
+   const date = new Date()
+   const options = {
+     day: 'numeric',
+     month: 'long'
+   }
+   return date.toLocaleDateString('ru-RU', options)
  }
 
  function setBgGreet() {
    let today = new Date(),
-     hour = today.getHours();
+     hour = today.getHours()
    if (hour < 5 || hour > 21) {
-     document.body.style.backgroundImage = `url(images/night/${Math.floor(Math.random()*20 + 1)}.jpg)`;
-     greeting.textContent = 'Бурной ночи,  ';
+     document.body.style.backgroundImage = `url(images/night/${Math.floor(Math.random()*20 + 1)}.jpg)`
+     greeting.textContent = 'Бурной ночи,  '
    } else if (hour < 11) {
-     document.body.style.backgroundImage = `url(images/morning/${Math.floor(Math.random()*20 + 1)}.jpg)`;
-     greeting.textContent = 'Доброе утречко,  ';
+     document.body.style.backgroundImage = `url(images/morning/${Math.floor(Math.random()*20 + 1)}.jpg)`
+     greeting.textContent = 'Доброе утречко,  '
    } else if (hour < 17) {
-     document.body.style.backgroundImage = `url(images/day/${Math.floor(Math.random()*20 + 1)}.jpg)`;
-     greeting.textContent = 'Хорошего дня,  ';
+     document.body.style.backgroundImage = `url(images/day/${Math.floor(Math.random()*20 + 1)}.jpg)`
+     greeting.textContent = 'Хорошего дня,  '
    } else {
-     document.body.style.backgroundImage = `url(images/evening/${Math.floor(Math.random()*20 + 1)}.jpg)`;
-     greeting.textContent = 'Веселого вечера,  ';
+     document.body.style.backgroundImage = `url(images/evening/${Math.floor(Math.random()*20 + 1)}.jpg)`
+     greeting.textContent = 'Веселого вечера,  '
    }
  }
 
  function setFocus(el) {
    if (el.type === 'keypress') {
      if (el.keyCode === 13) {
-          localStorage.setItem('focus', focusInput.value);
+       localStorage.setItem('focus', focusInput.value)
        focusText.innerText = localStorage.getItem('focus')
        focusInput.classList.add('hidden')
        focusText.classList.remove('hidden')
-       focusInput.blur();
+       focusInput.blur()
      }
    } else {
-         focusText.classList.add('hidden')
+     focusText.classList.add('hidden')
      focusInput.classList.remove('hidden')
-     localStorage.setItem('focus', focusInput.value);
+     localStorage.setItem('focus', focusInput.value)
    }
  }
 
  function setName(el) {
    if (el.type === 'keypress') {
      if (el.keyCode === 13) {
-       localStorage.setItem('name', nameInput.value);
+       localStorage.setItem('name', nameInput.value)
        nameText.innerText = localStorage.getItem('name')
        nameInput.classList.add('hidden')
        nameText.classList.remove('hidden')
-       nameInput.blur();
+       nameInput.blur()
      }
    } else {
      nameText.classList.add('hidden')
      nameInput.classList.remove('hidden')
-     localStorage.setItem('name', nameInput.value);
+     localStorage.setItem('name', nameInput.value)
    }
  }
 
  function newName(el) {
-   localStorage.setItem('name', '');
+   localStorage.setItem('name', '')
    nameInput.value = ''
    setName(el)
  }
 
  function blurName() {
+   localStorage.setItem('name', nameInput.value)
+   nameText.innerText = localStorage.getItem('name')
    nameInput.classList.add('hidden')
    nameText.classList.remove('hidden')
-   nameInput.blur();
+   nameInput.blur()
  }
 
  function newFocus(el) {
-   localStorage.setItem('focus', '');
+   localStorage.setItem('focus', '')
    focusInput.value = ''
    setFocus(el)
  }
 
  function blurFocus() {
+   localStorage.setItem('focus', focusInput.value)
+   focusText.innerText = localStorage.getItem('focus')
    focusInput.classList.add('hidden')
    focusText.classList.remove('hidden')
-   focusInput.blur();
+   focusInput.blur()
  }
 
  function newCity(el) {
-   localStorage.setItem('city', '');
+   localStorage.setItem('city', '')
    cityInput.value = ''
    setCity(el)
  }
 
  function blurCity() {
+   localStorage.setItem('city', cityInput.value)
+   cityText.innerText = localStorage.getItem('city')
    cityInput.classList.add('hidden')
    cityText.classList.remove('hidden')
-   cityInput.blur();
+   cityInput.blur()
  }
-
 
  function setName(el) {
    if (el.type === 'keypress') {
      if (el.keyCode === 13) {
-       localStorage.setItem('name', nameInput.value);
+       localStorage.setItem('name', nameInput.value)
        nameText.innerText = localStorage.getItem('name')
        nameInput.classList.add('hidden')
        nameText.classList.remove('hidden')
-       nameInput.blur();
+       nameInput.blur()
      }
    } else {
      nameText.classList.add('hidden')
      nameInput.classList.remove('hidden')
-     localStorage.setItem('name', nameInput.value);
+     localStorage.setItem('name', nameInput.value)
    }
  }
 
  function setCity(el) {
    if (el.type === 'keypress') {
      if (el.keyCode === 13) {
-       localStorage.setItem('city', cityInput.value);
+       localStorage.setItem('city', cityInput.value)
        cityText.innerText = localStorage.getItem('city')
        cityInput.classList.add('hidden')
        cityText.classList.remove('hidden')
-       cityInput.blur();
+       cityInput.blur()
      }
    } else {
      cityText.classList.add('hidden')
      cityInput.classList.remove('hidden')
-     localStorage.setItem('city', cityInput.value);
+     localStorage.setItem('city', cityInput.value)
    }
-   getWeather();
+   getWeather()
  }
 
  function getFocus() {
@@ -167,7 +175,7 @@
    } else {
      focusInput.classList.add('hidden')
      focusText.classList.remove('hidden')
-     focusText.textContent = localStorage.getItem('focus');
+     focusText.textContent = localStorage.getItem('focus')
    }
  }
 
@@ -178,7 +186,7 @@
    } else {
      nameInput.classList.add('hidden')
      nameText.classList.remove('hidden')
-     nameText.textContent = localStorage.getItem('name');
+     nameText.textContent = localStorage.getItem('name')
    }
  }
 
@@ -189,78 +197,73 @@
    } else {
      cityInput.classList.add('hidden')
      cityText.classList.remove('hidden')
-     cityText.textContent = localStorage.getItem('city');
+     cityText.textContent = localStorage.getItem('city')
    }
  }
 
-
-function changeBgr() {
-  const img = document.createElement('img');
- 
-  if (document.body.style.backgroundImage.includes('morning')) {
-    const src = `images/day/${Math.floor(Math.random()*20 + 1)}.jpg`
-    img.src = src;
-    img.onload =()=>{
-       document.body.style.backgroundImage = `url(${src})`;
+ function changeBgr() {
+   const img = document.createElement('img')
+   if (document.body.style.backgroundImage.includes('morning')) {
+     const src = `images/day/${Math.floor(Math.random()*20 + 1)}.jpg`
+     img.src = src
+     img.onload = () => {
+       document.body.style.backgroundImage = `url(${src})`
      }
-  } else if (document.body.style.backgroundImage.includes('day')) {
-    const src = `images/evening/${Math.floor(Math.random()*20 + 1)}.jpg`;
-    img.src = src;
-    img.onload =()=>{
-       document.body.style.backgroundImage = `url(${src})`;
+   } else if (document.body.style.backgroundImage.includes('day')) {
+     const src = `images/evening/${Math.floor(Math.random()*20 + 1)}.jpg`
+     img.src = src
+     img.onload = () => {
+       document.body.style.backgroundImage = `url(${src})`
      }
-  } else if (document.body.style.backgroundImage.includes('evening')) {
-    const src = `images/night/${Math.floor(Math.random()*20 + 1)}.jpg`;
-    img.src = src;
-    img.onload =()=>{
-       document.body.style.backgroundImage = `url(${src})`;
+   } else if (document.body.style.backgroundImage.includes('evening')) {
+     const src = `images/night/${Math.floor(Math.random()*20 + 1)}.jpg`
+     img.src = src
+     img.onload = () => {
+       document.body.style.backgroundImage = `url(${src})`
      }
-  } else if (document.body.style.backgroundImage.includes('night')) {
-    const src = `images/morning/${Math.floor(Math.random()*20 + 1)}.jpg`;
-    img.src = src;
-    img.onload =()=>{
-       document.body.style.backgroundImage = `url(${src})`;
+   } else if (document.body.style.backgroundImage.includes('night')) {
+     const src = `images/morning/${Math.floor(Math.random()*20 + 1)}.jpg`
+     img.src = src
+     img.onload = () => {
+       document.body.style.backgroundImage = `url(${src})`
      }
-  }
-}
-
-
+   }
+ }
 
  async function getQuote() {
-   const url = `quote.json`;
-   const res = await fetch(url);
-   const data = await res.json();
-   blockquote.textContent = data.quoteText[Math.floor(Math.random() * data.quoteText.length)];
-
+   const url = `quote.json`
+   const res = await fetch(url)
+   const data = await res.json()
+   blockquote.textContent = data.quoteText[Math.floor(Math.random() * data.quoteText.length)]
  }
 
  async function getWeather() {
-   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=ru&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
-   const res = await fetch(url);
-   const data = await res.json();
-   weatherIcon.className = 'weather-icon owf';
-   weatherIcon.classList.add(`owf-${data.weather[0].id}`);
-   temperature.textContent = `темпер ${data.main.temp}°C`;
-   wind.textContent = `ветер ${data.wind.speed} м/с`;
-   humidity.textContent = `влажность ${data.main.humidity} %`;
-   weatherDescription.textContent = data.weather[0].description;
+   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=ru&appid=08f2a575dda978b9c539199e54df03b0&units=metric`
+   const res = await fetch(url)
+   const data = await res.json()
+   weatherIcon.className = 'weather-icon owf'
+   weatherIcon.classList.add(`owf-${data.weather[0].id}`)
+   temperature.textContent = `темпер ${data.main.temp}°C`
+   wind.textContent = `ветер ${data.wind.speed} м/с`
+   humidity.textContent = `влажность ${data.main.humidity} %`
+   weatherDescription.textContent = data.weather[0].description
  }
 
 
 
- showTime();
- setBgGreet();
- getName();
- getFocus();
- getQuote();
+ showTime()
+ setBgGreet()
+ getName()
+ getFocus()
+ getQuote()
  getCity()
 
 
 
 
- bgReload.addEventListener('click', changeBgr);
- btnQuote.addEventListener('click', getQuote);
- document.addEventListener('DOMContentLoaded', getWeather);
+ bgReload.addEventListener('click', changeBgr)
+ btnQuote.addEventListener('click', getQuote)
+ document.addEventListener('DOMContentLoaded', getWeather)
  nameInput.addEventListener('keypress', setName)
  nameInput.addEventListener('blur', blurName)
  nameText.addEventListener('click', newName)
